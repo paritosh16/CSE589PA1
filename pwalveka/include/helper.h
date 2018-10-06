a506d693620fcfb35847864b6ac4cd4d612507d1
Paritosh Walvekar <paritosh.walvekar@gmail.com>
2018-10-06 19:02:23 -0400

    Bug fix.

--- pwalveka/include/helper.h
+++ pwalveka/include/helper.h
@@ -1,9 +1,10 @@
 #ifndef HELPER_H_
 #define HELPER_H_
 
+#include <vector>
 
 int author_command(char *result_string);
-int ip_command(char *device_hostname,char *device_ip_address);
-int tokenize_command(std::vector<char*>* input, char* cmd);
+int ip_command(char *device_hostname, char *device_ip_address);
+int tokenize_command(std::vector<char*>* tokenized_command, char* cmd);
 
 #endif
