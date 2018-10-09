901bdd5ebf1a07fa19121b776bfcf4585ebd3b1b
Paritosh Walvekar <paritosh.walvekar@gmail.com>
2018-10-09 12:11:25 -0400

    Bug fixed. Working on mac os.

--- pwalveka/src/server.cpp
+++ pwalveka/src/server.cpp
@@ -308,18 +308,10 @@ struct client_data add_new_client(int &fdsocket,struct sockaddr_in& client_addr)
   inet_ntop(AF_INET, &(client_addr.sin_addr), new_client.client_ip_address, len);
   // DEBUG BLOG
   int res = getnameinfo((struct sockaddr *)&client_addr, sizeof(client_addr), new_client.client_ip_address, sizeof(new_client.client_ip_address), NULL, 0, NI_NAMEREQD);
-  printf("The hostname of the client is : %s\n",new_client.client_ip_address);
-  char servicebuf[100];
   int hostname_from_addr = getnameinfo((struct sockaddr *)&client_addr, sizeof(client_addr), new_client.client_name, sizeof(new_client.client_name), NULL, 0, NI_NAMEREQD);
-   hostent* host_info = gethostbyname(new_client.client_name);
-   struct in_addr **addr_list = (struct in_addr **) host_info->h_addr_list;
-   for(int i = 0; addr_list[i] != NULL; i++)
-   {
-       // Return the first one;
-       strcpy(new_client.client_ip_address , inet_ntoa(*addr_list[i]) );
-       break;
-   }
-  printf("%d\n", ntohs(client_addr.sin_port));
+  printf("The ip address of the client is : %s\n", new_client.client_ip_address);
+  printf("The domain address of the client is : %s\n", new_client.client_name);
+  printf("Port to be entered in the vector:%d\n", ntohs(client_addr.sin_port));
   new_client.client_port = ntohs(client_addr.sin_port);
   new_client.message_sent = 0;
   new_client.message_recieved = 0;
